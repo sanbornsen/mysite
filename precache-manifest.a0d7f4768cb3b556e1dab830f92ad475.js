ecb085712b5358fbbdaf82aba3b5ea00f5d97fab
Sudipta Sen <sanborn.sen@gmail.com>
2020-07-12 16:04:01 +0530

    site upload

---
+++ precache-manifest.a0d7f4768cb3b556e1dab830f92ad475.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5b42b9cc2d578986afda44563787cc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c49ff532f5250618121",
+    "url": "/static/css/3.d54bb455.chunk.css"
+  },
+  {
+    "revision": "1548b82bf85439f0ac83",
+    "url": "/static/js/0.936a4dc5.chunk.js"
+  },
+  {
+    "revision": "8c49ff532f5250618121",
+    "url": "/static/js/3.f74f5277.chunk.js"
+  },
+  {
+    "revision": "5fe809f7c5187d2232bb1a98d278ed3e",
+    "url": "/static/js/3.f74f5277.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef924f6353d0a3a5a60e",
+    "url": "/static/js/4.24082c6a.chunk.js"
+  },
+  {
+    "revision": "e4a0d87becbcc6d09668875fe197b50f",
+    "url": "/static/js/4.24082c6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef01811ff69e04db1345",
+    "url": "/static/js/5.3df79af4.chunk.js"
+  },
+  {
+    "revision": "954990b9b448da967741",
+    "url": "/static/js/6.86d1ba3b.chunk.js"
+  },
+  {
+    "revision": "59ee70670411bcef8a0a",
+    "url": "/static/js/7.3f7d6633.chunk.js"
+  },
+  {
+    "revision": "021119ccbdb2c1c65f9d",
+    "url": "/static/js/8.e61cb62d.chunk.js"
+  },
+  {
+    "revision": "a55485c4513ffdb10e15",
+    "url": "/static/js/9.5dd3a19f.chunk.js"
+  },
+  {
+    "revision": "289986d936604036d458",
+    "url": "/static/js/main.7da2b21e.chunk.js"
+  },
+  {
+    "revision": "256c6efa9165c00fe7b2",
+    "url": "/static/js/runtime-main.01234642.js"
+  }
+]);
